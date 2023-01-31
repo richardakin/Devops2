e910d0b169bb42109c405d54535142bd3d1f4151
richard <richardakintewe@gmail.com>
2023-01-31 15:28:02 -0500

    test1

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ balance=10
 withdraw=5
 result=balance - withdraw
 # write test here
-assert result == 59, "Balance should be 5"
+assert result == 5, "Balance should be 5"
 
 
 
@@ -23,3 +23,32 @@ withdraw=1
 result=balance - withdraw
 # write test here
 assert result == 9, "Balance should be 9"
+
+
+# we withdraw 3
+# expect 7
+
+withdraw=3
+result=balance - withdraw
+# write test here
+assert result == 7, "Balance should be 7"
+
+
+# we withdraw 6
+# expect 4
+
+
+withdraw=6
+result=balance - withdraw
+# write test here
+assert result == 4, "Balance should be 4"
+
+
+# we withdraw 8
+# expect 2
+
+
+withdraw=8
+result=balance - withdraw
+# write test here
+assert result == 2, "Balance should be 2"
